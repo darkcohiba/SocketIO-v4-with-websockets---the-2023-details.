2662384480b1f553c5c2b67c37e3bd5bf9c88434
Sam Waters <samuel.waters@gmail.com>
2023-12-09 11:13:07 -0700

    native ws: built front end

--- native_ws/justWsServer.js
+++ native_ws/justWsServer.js
@@ -5,17 +5,26 @@ const websocket = require('ws')
 
 const server = http.createServer((req, res) =>{
 
-    res.end('This is coming from Just WS Server JS file!')
+    res.end('This is coming from Just WS Server JS file!');
 
 })
 
 const wss = new websocket.WebSocketServer({server})
 
+// headers happen before the handshake is complete
 wss.on('headers', (headers, req) => {
+    console.log(headers);
+})
 
-    console.log(headers)
-    
+// connection happens after the handshake is complete
+wss.on('connection', (ws, req)=>{
+    // console.log(ws);
 
+    // we are going to emit a message
+    ws.send('Welcome to the websocket server!')
+    ws.on('message',(data)=>{
+        console.log(data.toString())
+    })
 })
 
 server.listen(8000)
