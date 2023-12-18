18501f51497b364a6a615981ea620fce52e7750e
Sam Waters <samuel.waters@gmail.com>
2023-12-18 15:04:40 -0700

    chapter: basic chat app

--- basic_chat_app/chat.js
+++ basic_chat_app/chat.js
@@ -5,3 +5,12 @@ app.use(express.static(__dirname + '/public'))
 
 const expressServer = app.listen(8000);
 const io = socketio(expressServer);
+
+// io.on('connection', (socket)=>{
+//     console.log(socket.id, "has connected")
+//     // websockets we use the send method, socket io we use the emit method, message from server is a custom event
+//     socket.emit('messageFromServer', {data: "welcome to the socket server!"})
+//     socket.on('messageFromClient', (data)=>{
+//         console.log(data)
+//     })
+// })
