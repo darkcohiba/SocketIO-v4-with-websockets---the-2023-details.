bc6424051efb7637275b88186d9ae98fdd02e636
Sam Waters <samuel.waters@gmail.com>
2023-12-20 15:12:40 -0700

    chapter: basic chat app

--- basic_chat_app/chat.js
+++ basic_chat_app/chat.js
@@ -10,7 +10,8 @@ io.on('connection', (socket)=>{
     console.log(socket.id, "has connected")
     // websockets we use the send method, socket io we use the emit method, message from server is a custom event
     socket.emit('messageFromServer', {data: "welcome to the socket server!"})
-    socket.on('messageFromClient', (dataFromClient)=>{
-        console.log(dataFromClient)
+    socket.on('newMessageToServer', (dataFromClient)=>{
+        console.log("data: ", dataFromClient)
     })
+
 })
